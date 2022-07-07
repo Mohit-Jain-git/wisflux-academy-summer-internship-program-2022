962ec18ab7826df976770dccb6b682a4a1717c56
Mohit_Jain <55278084+Mohit-Jain-git@users.noreply.github.com>
2022-07-07 21:42:49 +0530

    Create Grade.js

---
+++ Day4/Sixth/Grade.js
@@ -0,0 +1,11 @@
+function exam_status(totmarks,is_exam)
+  {
+  if (is_exam) {
+    return totmarks >= 90;
+  }
+ return (totmarks >= 89 && totmarks <= 100);
+ }
+
+console.log(exam_status("85", " "));
+console.log(exam_status("59", "true "));
+console.log(exam_status("99", "false"));
